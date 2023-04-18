8e2ee306fd8d74aa9c5446dac5fbed372c3dfe92
Anik Barua <anikbb04@gmail.com>
2023-04-18 20:42:03 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from 'react-router-dom';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <></>
+    }
+  ])
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
