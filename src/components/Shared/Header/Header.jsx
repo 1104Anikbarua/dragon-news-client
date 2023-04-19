9b953642bb9b37e25dd527a6669665d3603c5ea5
Anik Barua <anikbb04@gmail.com>
2023-04-19 13:10:33 +0600

    make left navigation and load categories from server side

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import moment from 'moment';
 import logo from '../../../assets/logo.png'
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import Marquee from 'react-fast-marquee';
+import user from '../../../assets/user.png'
 const Header = () => {
     return (
         <Container>
@@ -13,34 +14,27 @@ const Header = () => {
             </div>
             <div className='bg-light d-flex'>
                 <Button variant='danger'>Latest</Button>
-                <Marquee speed={60} gradientWidth={100}>
+                <Marquee className='text-danger' speed={60} gradientWidth={100}>
+                    Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
                     Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
                 </Marquee>
             </div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#about">Career</Nav.Link>
+
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
+                            <Nav.Link href="#user">
+                                <img src={user} alt="" />
+                            </Nav.Link>
+                            <Nav.Link eventKey={2} href="#login">
+                                <Button variant='secondary'>Login</Button>
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
