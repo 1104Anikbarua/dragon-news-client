9b8b78bd7aba80bddc9f5fb61261904df2e3fb87
Anik Barua <anikbb04@gmail.com>
2023-04-21 21:04:38 +0600

    make right navigation card

--- src/components/LeftNavigation/LeftNavigation.jsx
+++ src/components/LeftNavigation/LeftNavigation.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Categories from '../Categories/Categories';
+import imageOne from '../../assets/1.png';
+import imageTwo from '../../assets/2.png';
+import imageThree from '../../assets/3.png';
+import moment from 'moment';
+import { HiOutlineCalendar } from 'react-icons/hi2';
 
 const LeftNavigation = () => {
     const [categories, setCategories] = useState([]);
@@ -21,6 +26,32 @@ const LeftNavigation = () => {
                         key={category.id}
                     ></Categories>)
                 }
+            </div>
+            <div>
+                <div>
+                    <img src={imageOne} alt="" />
+                    <p className='fw-bolder'>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+                    <div className='d-flex align-items-center justify-content-start text-secondary'>
+                        <p className='me-5'>Sports</p>
+                        <p><HiOutlineCalendar className='me-2'></HiOutlineCalendar>{moment().format('MMM D,YYYY')}</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={imageTwo} alt="" />
+                    <p className='fw-bolder'>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+                    <div className='d-flex align-items-center justify-content-start text-secondary'>
+                        <p className='me-5'>Sports</p>
+                        <p><HiOutlineCalendar className='me-2'></HiOutlineCalendar>{moment().format('MMM D,YYYY')}</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={imageThree} alt="" />
+                    <p className='fw-bolder'>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+                    <div className='d-flex align-items-center justify-content-start text-secondary'>
+                        <p className='me-5'>Sports</p>
+                        <p><HiOutlineCalendar className='me-2'></HiOutlineCalendar>{moment().format('MMM D,YYYY')}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
